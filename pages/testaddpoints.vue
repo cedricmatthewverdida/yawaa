4716090c390781620878bca33ae86b0539036a52
Cedric Matthew Verdida <chryhesmicazumic@gmail.com>
2021-07-19 14:57:26 +0800

    Create testaddpoints.vue

---
+++ pages/testaddpoints.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        HAHAHAHAHAHA
+    </div>
+</template>
+
+<script>
+    const Moralis = require("moralis")
+    import { mapState } from 'vuex'
+    export default {
+        computed:{
+            ...mapState(['pointbalance'])
+        },
+        mounted(){
+            const currentUser =  Moralis.User.current();
+            let addFunds = this.pointbalance + 1;
+            currentUser.set("balance",addFunds)
+            currentUser.save()
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
